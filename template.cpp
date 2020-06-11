3bea5d58c30cce5973ef21a705d7aa110b17a6dd
Md. Yamin <mohammadyamin80@gmail.com>
2020-06-11 20:28:05 +0600

    template edited

--- template.cpp
+++ template.cpp
@@ -34,9 +34,19 @@
 #define REP(i, j) FOR(i, 0, j, 1)
 #define RREP(i, j) RFOR(i, j, 0, 1)
 #define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
-#define INF 1e9
-#define MEM(a, b) memset(a, (b), sizeof(a))
-#define read(type) readInt<type>()
+#define INF (1e9)+7
+#define RESET(a, b) memset(a, (b), sizeof(a))
+#define lb lower_bound
+#define ub upper_bound
+#define newline pf("\n")
+#define sfd(n) sf("%lf", &n)
+#define sfll(n) sf("%lld", &n)
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define SIZE(v) (int)v.size()
+#define ALL(v) v.begin(), v.end()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
 
 using namespace std;
 
@@ -52,42 +62,6 @@ typedef set<int> seti;
 typedef multiset<int> mseti;
 
 
-/****** Template of Fast I/O Methods *********/
-template <typename T> inline void write(T x)
-{
-	int i = 20;
-	char buf[21];
-	// buf[10] = 0;
-	buf[20] = '\n';
-
-	do
-	{
-		buf[--i] = x % 10 + '0';
-		x/= 10;
-	}while(x);
-	do
-	{
-		putchar(buf[i]);
-	} while (buf[i++] != '\n');
-}
-
-template <typename T> inline T readInt()
-{
-	T n=0,s=1;
-	char p=getchar();
-	if(p=='-')
-		s=-1;
-	while((p<'0'||p>'9')&&p!=EOF&&p!='-')
-		p=getchar();
-	if(p=='-')
-		s=-1,p=getchar();
-	while(p>='0'&&p<='9') {
-		n = (n<< 3) + (n<< 1) + (p - '0');
-		p=getchar();
-	}
-
-	return n*s;
-}
 /************************************/
 
 int fx[] = {-1, 1, 0, 0};
@@ -106,13 +80,13 @@ int main() {
     
 
 
-    /* int tc;
-	tc = read(int);
+    /*int tc;
+	sf1(tc);
 
 	while(tc--){
 
-		//write(tc);
-	} */
+
+	}*/
 
     return 0;
 }
